69cdbe4794705cdf3fd5c969b879d5a16c689365
Kevin Wijaya <kevinwijaya306@gmail.com>
2023-12-20 04:24:55 -0500

    new

---
+++ medium/twopointer/threesum.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSum(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        result = []
+        for left in range(len(nums)-2):
+            if left > 0 and nums[left] == nums[left-1]:
+                continue
+            mid = left + 1
+            right = len(nums)-1
+            while mid < right:
+                currentsum = nums[left]+nums[mid] + nums[right]
+                if currentsum < 0:
+                    mid += 1
+                elif currentsum > 0:
+                    right -= 1
+                else:
+                    result.append([nums[left], nums[mid], nums[right]])
+                    while mid < right and nums[mid] == nums[mid+1]:
+                        mid += 1
+                    while mid < right and nums[right] == nums[right - 1]:
+                        right -= 1
+                    mid += 1
+                    right -= 1
+        return result
