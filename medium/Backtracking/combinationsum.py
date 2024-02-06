319b22b9c6004847bd6aa9f40527a7f0a90d0b0d
Kevin Wijaya <kevinwijaya306@gmail.com>
2024-02-06 13:33:26 -0500

    did another backtracking problem

---
+++ medium/Backtracking/combinationsum.py
@@ -0,0 +1,24 @@
+class Solution:
+    def combinationSum(self, candidates, target):
+        # This function will be called recursively to explore all possibilities
+        def backtrack(remaining, combination, start):
+            if remaining == 0:
+                # If we've hit the target, add a copy of the combination to the result
+                result.append(list(combination))
+                return
+            elif remaining < 0:
+                # If we've exceeded the target, there's nothing more to explore
+                return
+
+            # Loop through the candidates, starting from the current position
+            for i in range(start, len(candidates)):
+                # Add the current candidate to the combination
+                combination.append(candidates[i])
+                # Recursively try to find combinations from the current candidate onwards
+                backtrack(remaining - candidates[i], combination, i)
+                # Backtrack: remove the last element added and try the next candidate
+                combination.pop()
+
+        result = []
+        backtrack(target, [], 0)
+        return result
