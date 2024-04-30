23a48a0dc32c84ffb0b576a13542870d72781c29
Kevin Wijaya <kevinwijaya306@gmail.com>
2024-04-30 12:06:03 -0400

    wordsearch fix

--- dailies/wordsearch.py
+++ dailies/wordsearch.py
@@ -23,3 +23,37 @@ class Solution:
                     if dfs(r, c, 0):
                         return True
         return False
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        row = len(board)
+        col = len(board[0])
+        visited = set()
+
+        def dfs(r: int, c: int, charindex: int) -> bool:
+            if charindex == len(word):
+                return True
+            if (
+                r < 0
+                or c < 0
+                or row <= r
+                or col <= c
+                or board[r][c] != word[charindex]
+                or (r, c) in visited
+            ):
+                return False
+            visited.add((r, c))
+            res = (
+                dfs(r + 1, c, charindex + 1)
+                or dfs(r - 1, c, charindex + 1)
+                or dfs(r, c + 1, charindex + 1)
+                or dfs(r, c - 1, charindex + 1)
+            )
+            visited.remove((r, c))
+            return res
+
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0):
+                    return True
+        return False
