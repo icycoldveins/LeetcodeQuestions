d98ff799ee0350ce36ed84d22f94b945093dcdb7
Kevin Wijaya <kevinwijaya306@gmail.com>
2024-05-06 22:37:37 -0400

    double numbers linked list

--- Easy/1D-DynamicProgramming/mincostclimb.py
+++ Easy/1D-DynamicProgramming/mincostclimb.py
@@ -1,17 +1,28 @@
-class Solution(object):
-    def minCostClimbingStairs(self, cost):
-        n = len(cost)
-        dp = [0] * (n + 1)
+from typing import List
 
-        for i in range(2, n + 1):
-            option1 = dp[i - 1] + cost[i - 1]
-            option2 = dp[i - 2] + cost[i - 2]
-            dp[i] = min(option1, option2)
 
-        return min(dp[n], dp[n - 1])
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        cur = 0
+        dp0 = cost[0]
+        if len(cost) >= 2:
+            dp1 = cost[1]
+
+        for i in range(2, len(cost)):
+            cur = cost[i] + min(dp0, dp1)
+            dp0 = dp1
+            dp1 = cur
+
+        return min(dp0, dp1)
 
     def neetcode(self, cost: List[int]) -> int:
         cost.append(0)
         for i in range(len(cost) - 3, -1, -1):
             cost[i] += min(cost[i + 1], cost[i + 2])
         return min(cost[0], cost[1])
+
+
+solution = Solution()
+cost = [10, 15, 20]
+result = solution.minCostClimbingStairs(cost)
+print("Number of distinct ways to climb to the top with", cost, "steps:", result)
--- medium/DynamicProgramming1D/CoinchangesBU.py
+++ medium/DynamicProgramming1D/CoinchangesBU.py
@@ -32,39 +32,49 @@ class Solution(object):
         # Step 5: Return the minimum number of coins needed to make up the given 'amount'.
         return dp[amount]
 
+
 class Solution(object):
     def coinChangeTD(self, coins, amount):
         # Create a memoization dictionary to store computed results.
         memo = {}
-        
+
         def dp(target_amount):
             # Check if the result for this amount is already computed.
             if target_amount in memo:
                 return memo[target_amount]
-            
+
             # Base case: If the target amount is 0, no coins are needed.
             if target_amount == 0:
                 return 0
-            
+
             # Initialize the minimum number of coins needed as a large value.
             min_coins = float('inf')
-            
+
             # Iterate through each coin denomination.
             for coin in coins:
                 # Check if the current coin can be used to make up the target amount.
                 if target_amount >= coin:
                     # Recursively calculate the minimum number of coins needed.
                     subproblem = dp(target_amount - coin)
-                    
+
                     # Update the minimum number of coins.
                     if subproblem != -1:
                         min_coins = min(min_coins, subproblem + 1)
-            
+
             # If min_coins is still infinity, no valid combination was found.
             # Otherwise, memoize the result and return it.
-            memo[target_amount] = min_coins if min_coins != float('inf') else -1
+            memo[target_amount] = min_coins if min_coins != float(
+                'inf') else -1
             return memo[target_amount]
-        
+
         # Start the recursive process from the target amount.
         result = dp(amount)
         return result
+
+    def coinchangeneetcode(self, coins, amount):
+        dp = [amount+1] * (amount+1)
+        dp[0] = 0
+        for a in range(1, amount+1):
+            for c in coins:
+                if a - c >= 0:
+                    dp[a] = min(dp[a], 1)
