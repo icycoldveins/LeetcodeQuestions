83ebe3ba3e9097ea0812bf2e2ab9170b509045e1
Kevin Wijayaclear <you@example.com>
2024-05-15 12:03:26 -0400

    new ways to decode

--- dailies/decodeways.py
+++ dailies/decodeways.py
@@ -92,3 +92,17 @@ Now, `dp` looks like `[1, 1, 3, 4]`.
 		if 10 <= int(s[i-2:i]) <= 26: #(3)
 			dp[i] += dp[i - 2]
 	return dp[len(s)]"""
+
+
+# neetcode solution
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = {len(s): 1}
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            if i+1 < len(s) and (s[i] == "1" or s[i] == "2" and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
+        return dp[0]
